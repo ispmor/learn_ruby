9e00acf8bd27ba62b3ff90b39ec320df92e6fc93
ispmor <puszkarski.bartosz@gmail.com>
2017-07-10 13:41:24 +0200

    solved calc with bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -12,6 +12,34 @@ def sum (tab)
 		a+=i		
 	end
 	return a
-	
+end
+def multiply(tab)
+	a=1
+	tab.each do |i|
+		a*=i
+	end
+	return a
+end
+def power(x,y)
+	a=x
+	while y >1 do
+ 	a*=x
+ 	y-=1
+	end
+	return a
+end
+def factorial(x)
+	if x==0
+		return 1
+	else
+		a =1
+		while x>1 do
+
+			a*=x
+
+			x-=1
+		end
+		return a
+	end
 	
 end
