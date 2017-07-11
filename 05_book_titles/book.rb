68c9ab8d45a771f246d895c722b2f36d130fc5fb
ispmor <puszkarski.bartosz@gmail.com>
2017-07-11 19:25:27 +0200

    solved 05 - book title

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+	
+attr_accessor :title
+	def title
+		my_book_title = @title
+		my_book_title = my_book_title.split(" ")
+		no_of_words = my_book_title.size
+		no_of_words.times do |i|
+		if i == 0	
+			my_book_title[i].capitalize!
+		else		
+			if my_book_title[i] != 'and' && my_book_title[i] != 'of' && my_book_title[i] != 'in' && my_book_title[i] != 'the' && my_book_title[i] != 'on' && my_book_title[i] != 'until' && my_book_title[i] != 'till' && my_book_title[i] != 'for'
+					if  my_book_title[i] != 'a' && my_book_title[i] != 'an'
+						my_book_title[i].capitalize!
+					end
+				end
+			end
+		end
+
+		@title=my_book_title.join(" ")
+		
+	end
 end
